4181d1ab007ea42ccacbfddf250c26dad8d05a29
Nicholas <nick.orsi1992@gmail.com>
2023-11-27 15:59:50 -0800

    Added functionality to files 1-6.

--- 03_last_element.py
+++ 03_last_element.py
@@ -12,3 +12,6 @@ def last_element(lst):
         >>> nums == [1, 2, 3]
         True
     """
+    if len(lst) != 0:
+        return lst[-1]
+    return None
--- 06_single_letter_count.py
+++ 06_single_letter_count.py
@@ -1,12 +1,20 @@
 def single_letter_count(word, letter):
     """How many times does letter appear in word (case-insensitively)?
-    
+
         >>> single_letter_count('Hello World', 'h')
         1
-        
+
         >>> single_letter_count('Hello World', 'z')
         0
-        
+
         >>> single_letter_count("Hello World", 'l')
         3
     """
+    word_upper_case = word.upper()
+    letter_upper_case = letter.upper()
+    letter_count = 0
+
+    for letter in word_upper_case:
+        if letter == letter_upper_case:
+            letter_count += 1
+    return letter_count
